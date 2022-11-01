3b139a66126cfe99b41fcbdccd3bcf8e6f4b08c6
Sabuto <robe_dunne@hotmail.com>
2022-11-01 19:30:16 +0000

    add tailwind etc

---
+++ resources/views/Layouts/header.blade.php
@@ -0,0 +1,9 @@
+<div class="w-full z-50 top-0 py-3 sm:py-5 absolute">
+    <div class="container flex items-center justify-between">
+        <div>
+            <a href="/">
+                Logo
+            </a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Demo - @yield('title')</title>
+</head>
+<body>
+
+    @yield('content')
+</body>
+</html>

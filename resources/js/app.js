c9b1f538401fadfcb491dca22b1397ded6c95e5e
Sabuto <robe_dunne@hotmail.com>
2022-11-02 19:19:02 +0000

    add lightbox to portfolio

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
 import './bootstrap';
+
+import 'spotlight.js/dist/js/spotlight.min'
--- resources/views/Pages/portfolio.blade.php
+++ resources/views/Pages/portfolio.blade.php
@@ -1,5 +1,52 @@
 @extends('layout')
 @section('title', 'Portfolio')
 @section('content')
+    <div class="container mx-auto lg:grid lg:grid-cols-3 lg:gap-2">
+        <div class="w-full rounded">
+            <a class="spotlight" href="https://cdn.pixabay.com/photo/2022/06/03/03/50/beach-7239311__340.jpg"
+               data-src-800="https://cdn.pixabay.com/photo/2022/06/03/03/50/beach-7239311__340.jpg"
+               data-src-1200="https://cdn.pixabay.com/photo/2022/06/03/03/50/beach-7239311__340.jpg"
+               data-src-2400="https://cdn.pixabay.com/photo/2022/06/03/03/50/beach-7239311__340.jpg"
+               data-src-3800="https://cdn.pixabay.com/photo/2022/06/03/03/50/beach-7239311__340.jpg">
+                <img src="https://cdn.pixabay.com/photo/2022/06/03/03/50/beach-7239311__340.jpg">
+            </a>
+        </div>
+        <div class="w-full rounded">
+            <a class="spotlight" href="https://cdn.pixabay.com/photo/2022/06/06/17/56/flowers-7246619__340.jpg"
+               data-src-800="https://cdn.pixabay.com/photo/2022/06/06/17/56/flowers-7246619__340.jpg"
+               data-src-1200="https://cdn.pixabay.com/photo/2022/06/06/17/56/flowers-7246619__340.jpg"
+               data-src-2400="https://cdn.pixabay.com/photo/2022/06/06/17/56/flowers-7246619__340.jpg"
+               data-src-3800="https://cdn.pixabay.com/photo/2022/06/06/17/56/flowers-7246619__340.jpg">
+                <img src="https://cdn.pixabay.com/photo/2022/06/06/17/56/flowers-7246619__340.jpg">
+            </a>
+        </div>
+        <div class="w-full rounded">
+            <a class="spotlight" href="https://cdn.pixabay.com/photo/2022/06/22/18/28/deer-7278490__340.jpg"
+               data-src-800="https://cdn.pixabay.com/photo/2022/06/22/18/28/deer-7278490__340.jpg"
+               data-src-1200="https://cdn.pixabay.com/photo/2022/06/22/18/28/deer-7278490__340.jpg"
+               data-src-2400="https://cdn.pixabay.com/photo/2022/06/22/18/28/deer-7278490__340.jpg"
+               data-src-3800="https://cdn.pixabay.com/photo/2022/06/22/18/28/deer-7278490__340.jpg">
+                <img src="https://cdn.pixabay.com/photo/2022/06/22/18/28/deer-7278490__340.jpg">
+            </a>
+        </div>
 
+        <div class="w-full rounded">
+            <a class="spotlight" href="https://cdn.pixabay.com/photo/2022/01/04/16/01/lighthouse-6915406__340.jpg"
+               data-src-800="https://cdn.pixabay.com/photo/2022/01/04/16/01/lighthouse-6915406__340.jpg"
+               data-src-1200="https://cdn.pixabay.com/photo/2022/01/04/16/01/lighthouse-6915406__340.jpg"
+               data-src-2400="https://cdn.pixabay.com/photo/2022/01/04/16/01/lighthouse-6915406__340.jpg"
+               data-src-3800="https://cdn.pixabay.com/photo/2022/01/04/16/01/lighthouse-6915406__340.jpg">
+                <img src="https://cdn.pixabay.com/photo/2022/01/04/16/01/lighthouse-6915406__340.jpg">
+            </a>
+        </div>
+        <div class="w-full rounded">
+            <a class="spotlight" href="https://cdn.pixabay.com/photo/2022/06/19/07/12/mount-kilimanjaro-7271184__340.jpg"
+               data-src-800="https://cdn.pixabay.com/photo/2022/06/19/07/12/mount-kilimanjaro-7271184__340.jpg"
+               data-src-1200="https://cdn.pixabay.com/photo/2022/06/19/07/12/mount-kilimanjaro-7271184__340.jpg"
+               data-src-2400="https://cdn.pixabay.com/photo/2022/06/19/07/12/mount-kilimanjaro-7271184__340.jpg"
+               data-src-3800="https://cdn.pixabay.com/photo/2022/06/19/07/12/mount-kilimanjaro-7271184__340.jpg">
+                <img src="https://cdn.pixabay.com/photo/2022/06/19/07/12/mount-kilimanjaro-7271184__340.jpg">
+            </a>
+        </div>
+    </div>
 @endsection

1a17f0de6bde29be590ee92818b38e6fb413fe85
Sabuto <robe_dunne@hotmail.com>
2022-11-07 19:26:51 +0000

    update deploy

--- deploy.php
+++ deploy.php
@@ -2,6 +2,7 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
+require 'contrib/yarn.php';
 
 // Config
 
@@ -17,5 +18,14 @@ host('139.162.215.40')
     ->set('remote_user', 'deployer')
     ->set('deploy_path', '~/dd');
 
+// Tasks
+
+desc('builds the site');
+task('yarn:build', function () {
+    run("cd {{release_path}} && {{bin/yarn}} build");
+});
+
 // Hooks
+after('deploy:update_code', 'yarn:install');
+after('deploy:vendors', 'yarn:build');
 after('deploy:failed', 'deploy:unlock');
